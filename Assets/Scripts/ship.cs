47fcd9b666b7b3c3618d4b5a239722cebd10fb4c
Sebastian Anttury <antturysebastian@gmail.com>
2020-05-12 21:46:51 -0500

    Se añaden los archivos del repositorio anterior

---
+++ Assets/Scripts/ship.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship : MonoBehaviour
+{
+
+    
+
+    //Codigo para eliminar Objetos
+    private void OnTriggerEnter2D(Collider2D other){
+
+            if(other.name=="Naranja" || other.name=="Pera" ||
+            other.name=="Banano" || other.name=="Insecto"){
+
+            
+            Debug.Log("Detectado");
+            Destroy(other.gameObject);
+            }
+    }
+
+    
+
+
+}
