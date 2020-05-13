9f1e7da3573cd98861fe440af11ba776840e2a6a
020697Dmv <dmaciasv@uqvirtual.edu.co>
2020-05-13 14:56:53 -0500

    Documentacion
    13-05-2020

--- Assets/Tests/Test.cs
+++ Assets/Tests/Test.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-
+//Clase donde se estan los metodos de las pruebas unitarias
 namespace Tests {
     public class Test {
         // A Test behaves as an ordinary method
+
+        /*Prueba Cubo
+        */
+
         [Test]
         public void Test1 () {
-            //Este test no tiene sentido solo se criea un cubo y ya jajajaj
             GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
             cube.AddComponent<Rigidbody> ();
             cube.transform.position = new Vector3 (5, 5, 0);
             Assert.True (cube != null);
         }
-
+        //Crea un objeto tipo Roca el cual se toma de Prefabs
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
@@ -27,6 +30,8 @@ namespace Tests {
             Assert.True (game.player == null);
             yield return null;
         }
+        /*Crea un objeto tipo Enemigo el cual se toma de Prefabs
+        La posicion esta definida antes*/
         [UnityTest]
         public IEnumerator Tes3 () {
             GameObject gameGameObject = MonoBehaviour.Instantiate
@@ -36,23 +41,27 @@ namespace Tests {
             Assert.True (game.initialPosition == new Vector3(0,0,0));
             yield return null;
         }
+
+        /*Crea un Game Object
+         */
          [UnityTest]
         public IEnumerator Test4 () {
             GameObject gameGameObject = MonoBehaviour.Instantiate
              (Resources.Load<GameObject> ("Prefabs/Enemy"));
             Enemy game = gameGameObject.GetComponent<Enemy> ();
-//que prueba se le ocuurre? Yo tenia pensado esta
             Assert.True (game.player==null);
             yield return null;
         }
+
+        /*Este metodo prueba el objeto ItemWorld 
+          el cual no tiene script  
+        */
            [UnityTest]
         public IEnumerator Test5 () {
             GameObject gameGameObject = MonoBehaviour.Instantiate
              (Resources.Load<GameObject> ("Prefabs/ItemWorld"));
             BoxCollider2D b=  gameGameObject.GetComponent<BoxCollider2D> ();
               Debug.Log(b);
-// pues no se como obtener una colision sin un script,pero se puede verificar que el boxcoll
-//ider de ese gameobject sea un trigger
             Assert.True (b.isTrigger);
             yield return null;
         }
